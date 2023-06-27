b040e4f2d46ac9043b8d0215c6079b0dd83635a5
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-28 01:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+document.getElementById("form1").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
 
-}
+  // Get the input values
+  var firstName = document.getElementById("fname").value;
+  var lastName = document.getElementById("lname").value;
+
+  // Display the first and last name using alert()
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
+});
